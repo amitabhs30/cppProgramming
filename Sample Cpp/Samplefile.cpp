ac8a3daa57fab284371dfcf8f93d205fd9354889
Amitabh Sharma <amitabhs30@gmail.com>
2020-12-01 10:35:03 +0530

    Created a Sample cpp file for any compititions

--- Sample Cpp/Samplefile.cpp
+++ Sample Cpp/Samplefile.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
 using namespace std;
 
 int main()
 {
-    string input;
-    cin>>input;
-    
-    
+    int z;
+    cin>>z;
+    while((z--)>0)
+    {
+        //All the best!!
+    }
     return 0;
 }
