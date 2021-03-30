762c34d523c9446a43c124d04aeacc46f9a0c74c
Amitabh Sharma <amitabhs30@gmail.com>
2021-03-30 11:47:59 +0530

    created simple cpp boilerplate

---
+++ practiceQuestions/simpleCppBoilerplate.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<map>
+
+using namespace std;
+
+
+void solve(){
+
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
